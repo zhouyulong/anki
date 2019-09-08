c8dc15835155b310bd7bb71416853742e5fb30d2
zhou <wudixiaofeiji@outlook.com>
2019-09-09 02:27:13 +0800

    v0.1 -- result有问题，还无法正常导入

--- lodashOfAnki.js
+++ lodashOfAnki.js
@@ -30,58 +30,59 @@ const originData = async (url) => {
 let res = async () => {
     let data = await originData('http://lodash.think2011.net/all')
     let $ = await cherrio.load(data)
+    let index = 1
     let result = ''
-    let length = $('div[data-render-page="entry"]').each(function (index) {
+    // 通过功能记忆名字
+    let ankiCardTxtFunctToName = ''
+    // 通过名字记忆功能、使用方法
+    let ankiCardTxtNameToFunct = ''
+    $('div[data-render-page="entry"]').each(function (i) {
         let h2Text = $(this).children('h2').text().split(' ').slice(0, 1).join('')  // 每一项标题
-        result += h2Text
-        result += '\n'
-        console.log(h2Text)
         let name = $(this).children('div .signature').text() // 函数声明
-        result += name
-        result += '\n'
-        console.log(name)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + (index++).toString() + '、' + name + '\t'
         let interpretation = _.replace($(this).children('div[data-render-html="/' + h2Text + '"]').text(), '\n', '').replace('\n', '')
         while (_.includes(interpretation, '\n')) {
            interpretation = _.replace(interpretation, '\n', '')
         }
-        result += interpretation
-        result += '\n'
-        console.log(interpretation)
+        ankiCardTxtNameToFunct += '"<div>'
+        ankiCardTxtNameToFunct += interpretation
+        ankiCardTxtNameToFunct += '</div><div><font color=""#0000ff""><b>'
+        ankiCardTxtFunctToName = ankiCardTxtFunctToName +  (index++).toString() + '、'
+        ankiCardTxtFunctToName += interpretation
+        ankiCardTxtFunctToName += '\t'
+        ankiCardTxtFunctToName += '"' + name + '"\n'
         let h3Text = $(this).children('h3').text().slice(0,2)
-        result += h3Text
-        result += '\n'
-        console.log(h3Text)
+        ankiCardTxtNameToFunct += h3Text
+        ankiCardTxtNameToFunct += '</b></font></div>'
         let paramsText = $(this).children('ol').text()
         while (_.includes(paramsText, '\n')) {
             paramsText = _.replace(paramsText, '\n', '')
         }
-        result += paramsText
-        result += '\n'
-        console.log(paramsText)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + '<div>' + paramsText + '</div>'
         let h3Return = $(this).children('h3').text().slice(2, -2) // 返回值
-        result += h3Return
-        result += '\n'
-        console.log(h3Return)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + '<div><font color=""#0000ff""><b>' + h3Return + '</b></font></div>'
         let returnContent = _.replace($(this).children('div .returns').text()).replace('\n', '')
-        result += returnContent
-        result += '\n'
-        console.log(returnContent)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + '<div>' + returnContent + '</div>'
         let exampleTitle = $(this).children('h3').text().slice(-2)  // 示例
-        result += exampleTitle
-        result += '\n'
-        console.log(exampleTitle)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + '<div><font color=""#a9a9a9""><b>' + exampleTitle + '</b></font></div>'
         let exampleContent = $(this).children('div[data-render-html="/#synthetic"]').text()
-        result += exampleContent
-        result += '\n----itemEnd-\n'
-        console.log(exampleContent)
+        ankiCardTxtNameToFunct = ankiCardTxtNameToFunct + '<div>' + exampleContent + '</div>"\n'
+        result = result + ankiCardTxtNameToFunct + ankiCardTxtFunctToName + '\n-----------------------------------\n'
+        console.log(ankiCardTxtNameToFunct)
+        console.log(ankiCardTxtFunctToName)
+        console.log('--------------------------')
     })
     // 写入文件
     fs.open('./temp.txt', 'w', 0o666, (err, fd) => {
         if (err) {
             console.log(err)
         } else {
-            fs.writeFile(fd, result, (err) => {
-                console.log(err)
+            fs.writeFile(fd, ankiCardTxtNameToFunct, (err) => {
+                if (err) {
+                    console.log('err', err)
+                } else {
+                    console.log('-----end-----')
+                }
             })
         }
     })
